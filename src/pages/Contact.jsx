5c54e31286c07e9ae9b7e40e0be2b49fab39c806
Happy <caovandong20@gmail.com>
2025-09-20 18:22:07 +0700

    useEffect in project

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import styles from "../assets/css/Contact.module.css";
 import PhoneInput from "react-phone-input-2";
