39febf2559a1859dcfe1b73b9e87b345fff78c91
Dong <caovandong20@gmail.com>
2025-09-29 10:46:07 +0700

    Upload session create user in project latest 1

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -6,6 +6,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiFetch = axios.create({
     baseURL : API_URL,
+    withCredentials: true,
     headers: {
         "Content-Type": "application/json"
     }
